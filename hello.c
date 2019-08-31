89ad582ed77db384b08381173d6ffae4347b5e2c
APPOLLO7 <54381887+APPOLLO7@users.noreply.github.com>
2019-08-30 21:32:38 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+int main(int argc, char **argv) {
+
+	printf("Apollinaire Amomba  %s \n", str);
+
+		//     printf("computer science\n");
+
+		//     printf("maths\n");
+
+		//     printf("Justin Kerr\n");
+
+		//     printf("Computer Engineering\n");
+
+		//     printf("Physics\n");
+
+return 0;
+
+}
